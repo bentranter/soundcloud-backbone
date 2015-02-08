78dac3ed97c0fb21cebddc1ed4c0206f186bb7a9
Ben Tranter <bwtrante@lakeheadu.ca>
2015-02-08 17:39:36 -0500

    Add profile info from API

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
 'use strict';
 
-// Make a model for all the crap Soundcloud gives us
+// Make a model for all the crap Soundcloud gives us, and the profile info
 var Songs = Backbone.Model.extend();
+var Profile = Backbone.Model.extend();
 
 // Make a collection to hold the data in
 var SongsCollection = Backbone.Collection.extend({
 	model: Songs,
 	url: 'https://api.soundcloud.com/users/33748259/tracks.json?client_id=8ec20fb5cf443b6a8370954c522149c3' // Grab Keenan's songs, make no attempt to hide my client ID
+});
+
+// The user's profile info
+var ProfileInfo = Backbone.Collection.extend({
+	model: Profile,
+	url: 'https://api.soundcloud.com/users/33748259?client_id=8ec20fb5cf443b6a8370954c522149c3'
+});
+
+var ProfileView = Backbone.View.extend({
+	el: '#profile',
+	template: _.template($('#profileTemplate').html()),
+	initialize: function() {
+		this.collection.fetch({ reset: true });
+		this.listenTo(this.collection, 'reset', this.render);
+	},
+	render: function() {
+		this.collection.each(function(model) {
+			var profileTemplate = this.template(model.toJSON());
+			this.$el.append(profileTemplate);
+		}, this);
+	}
+});
+
+var profileInfo = new ProfileInfo();
+var profileView = new ProfileView({
+	collection: profileInfo
 });
 
 // Setup a view
@@ -34,3 +61,9 @@ var songsCollection = new SongsCollection();
 var songsView = new SongsView({
 	collection: songsCollection
 });
+
+// Handle audio element
+setTimeout(function() {
+	var audio = document.getElementsByTagName('audio')[1];
+	console.log(audio.src);
+}, 3000);
